83285a7193a58c09947b360547735515e3984789
AnbazhaganCode <151020067+AnbazhaganCode@users.noreply.github.com>
2023-11-30 22:23:35 +0530

    Update CodeController.cs

--- ASP NET CORE  MVC Project/Controllers/CodeController.cs
+++ ASP NET CORE  MVC Project/Controllers/CodeController.cs
@@ -1,10 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_NET_CORE__MVC_Project.Controllers
 {
     public class CodeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Java()
+        {
+            return View();
+        }
+
+        public IActionResult CSharp()
+        {
+            return View();
+        }
+
+        public IActionResult C()
         {
             return View();
         }
